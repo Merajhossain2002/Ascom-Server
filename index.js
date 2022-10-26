3f0beb78eb95bf0ab6cdb18a82d28b4b21b711f5
Meraj Faizan <mdmaraj1500@gmail.com>
2022-10-26 10:44:17 +0600

    json data added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+const courses = require("./data/courses.json");
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
