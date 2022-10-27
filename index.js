0c927b3ebf5bc0f17a54d0e43d6bad7dc0767206
Meraj Faizan <mdmaraj1500@gmail.com>
2022-10-27 18:57:00 +0600

    new premium json added

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require("./data/courses.json");
+const course = require("./data/singleCourse.json");
+const premium = require("./data/premium.json");
 
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/singleCourse/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = course.find((c) => c.id == id);
+  res.send(selectedCourse);
+});
+
+app.get("/checkout/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedPremium = premium.find((p) => p.id == id);
+  res.send(selectedPremium);
 });
 
 app.listen(port, () => {
